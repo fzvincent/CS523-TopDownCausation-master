36c4aa33a65fe42afbd6247f146db1860cc76018
liangkun <179639879@qq.com>
2022-02-23 17:17:57 -0700

    restore myTE

--- Significant_TE.R
+++ Significant_TE.R
@@ -1,7 +1,7 @@
 setwd("d:/Yu/Projects/logical/CS523-TopDownCausation-master")
 epsCount <- 48
 metaCount <- 10
-
+historyDepth <- 4
 #BU_TE <- TD_TE <- array(dim=c(epsCount+1))
 BU_TE05 <- TD_TE05 <- rep(0, epsCount)
 BU_TE10 <- TD_TE10 <- rep(0, epsCount)
@@ -25,8 +25,8 @@ for ( i in 1:epsCount) {
     filename <- paste('TEdata/TEdata_MX_',i-1,'_',j,'.csv',sep = '')
     lm_data <- read.csv(filename)
     
-    for (k in 1:4){
-      for (depth in range(1:4))
+    for (k in 2:4){  # 2-4 corresponding to the 3 random picked sub-groups, 1st column is mean-field
+      for (depth in range(1:historyDepth))
         
         TE_temp <- transfer_entropy(lm_data[1], lm_data[k], 
                          lx =depth, ly = depth)
@@ -51,7 +51,7 @@ for ( i in 1:epsCount) {
   }
 }
 
-Eps <- seq(1:48)
+Eps <- seq(1:epsCount)
 par(mfrow = c(2,1))
 plot(Eps, TD_TE05)
 plot(Eps, BU_TE05)
